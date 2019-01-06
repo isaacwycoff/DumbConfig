1a35976c3241e12ddad6579523750c20615e50f6
Isaac Wycoff <isaac.wycoff@gmail.com>
2019-01-05 22:43:25 -0800

    Initial commit

---
+++ DumbConfig/Exceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DumbConfig
+{
+	public class SettingNotFoundException : Exception
+	{
+		public string Name { get; set; }
+
+		public SettingNotFoundException(string name)
+			: base("Couldn't find setting with that name.")
+		{
+			Name = name;
+		}
+	}
+
+	public class InvalidSettingTypeException : Exception
+	{
+		public Type Type { get; set; }
+
+		public InvalidSettingTypeException(Type type)
+			: base ("Invalid type")
+		{
+			Type = type;
+		}
+	}
+}
---
+++ DumbConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DumbConfig")]
+[assembly: AssemblyDescription("Some helper classes for importing things from app.config & web.config")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DumbConfig")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("Isaac Wycoff")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4202c1e2-2e92-4feb-b88f-e2b3b449914c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
